461c2f1591957cd8d23ce1a51f841d0090a82502
بندر لڑکا <Davidyanni1406@gmail.com>
2023-07-25 16:37:07 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+from ethical_concerns import get_company_overviews, analyze_ethical_concerns
+
+app = Flask(__name__)
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    data = request.get_json()
+    ethical_concerns = data.get('ethical_concerns', [])
+    companies = data.get('companies', [])
+
+    # Get company overviews from Alpha Vantage API
+    company_overviews = get_company_overviews(companies)
+
+    # Perform analysis with OpenAI API
+    analysis_results = analyze_ethical_concerns(ethical_concerns, company_overviews)
+
+    return jsonify(analysis_results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
