6538e8b88986a123e729aaf143c7d6ea4d340ef1
dyanni3 <davidyanni1406@gmail.com>
2023-07-26 12:29:48 -0400

    updates

--- ethical_concerns.py
+++ ethical_concerns.py
@@ -1,16 +1,20 @@
 import requests
 from config import ALPHA_VANTAGE_API_KEY, OPENAI_API_KEY
 
+ALPHA_VANTAGE_BASE_URL = "https://www.alphavantage.co/query?"
+
 # Function to fetch company overviews from Alpha Vantage API
 def get_company_overviews(companies):
     company_overviews = {}
 
+    query_endpoint = ALPHA_VANTAGE_BASE_URL + "function=OVERVIEW"
+
     for company in companies:
-        url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={company}&apikey={ALPHA_VANTAGE_API_KEY}'
+        url = query_endpoint + f'&symbol={company}&apikey={ALPHA_VANTAGE_API_KEY}'
         response = requests.get(url)
         if response.status_code == 200:
             company_overview = response.json()
-            company_overviews[company] = company_overview
+            company_overviews[company] = company_overview['Description']
 
     return company_overviews
 
